44649a5a83d55611d9b771b0145bd9762d01cc98
Baehyeok <85778314+Baehyeok@users.noreply.github.com>
2021-08-07 20:34:07 +0900

    Update Oracle_DAO.py

--- src/Oracle_DAO.py
+++ src/Oracle_DAO.py
@@ -12,60 +12,71 @@ class OracleDao:
         self.cursor.close()
         self.conn.close()
         
-    def CreateTable(self): 
+    def CreateTable(self, tableName): 
         drop_sql = """
         BEGIN
-            EXECUTE IMMEDIATE 'DROP TABLE videos';
+            EXECUTE IMMEDIATE 'DROP TABLE video_%s';
         EXCEPTION
             WHEN OTHERS THEN
                 IF SQLCODE != -942 THEN
                     RAISE;
                 END IF;
         END;
-        """
+        """%(tableName)
         self.cursor.execute(drop_sql)
         
         create_sql = ("""
-            CREATE TABLE videos(
+            CREATE TABLE video_%s(
                 link VARCHAR2(50),
                 time VARCHAR2(10),
                 title VARCHAR2(50),
                 views VARCHAR2(10),
                 origin VARCHAR2(10)
                 ) 
-        """)
+        """%(tableName))
         self.cursor.execute(create_sql)
-        create_seq = """CREATE SEQUENCE videos_seq START WITH 1 INCREMENT BY 1 MINVALUE 1 MAXVALUE 99999999"""
-        self.cursor.execute(create_seq)
+        # create_seq = """CREATE SEQUENCE videos_seq START WITH 1 INCREMENT BY 1 MINVALUE 1 MAXVALUE 99999999"""
+        # self.cursor.execute(create_seq)
 
-    def insertVideo(self, tableName, link, time, title, view, origin):
+    def insertVideo(self, tableName, link, time, title, views, origin):
+        """
+        데이터 사입 함수
+        tableName = 테이블 이름,
+        link = 링크
+
+        """
         insert_sql = """
-            INSERT INTO video_%s (link, time, title, view, origin) values('%s', '%s', '%s', '%s', '%s');
-        """%(tableName, link, time, title, view, origin)
+            INSERT INTO video_%s (link, time, title, views, origin) values('%s', '%s', '%s', '%s', '%s')
+        """%(tableName, link, time, title, views, origin)
         self.cursor.execute(insert_sql)
         self.conn.commit()
 
     def searchVideo(self):
-        "테이블 명으로 파일을 나누고 검색한다"
-        search_sql = 'SELECT *  FROM all_tables'
+        """테이블 명으로 파일을 나누고 검색한다
+        """
+        search_sql = "select object_name from user_objects where object_type = 'TABLE'"
             
         
         self.cursor.execute(search_sql)
-        return self.cursor.fetchall()
+        print(self.cursor.fetchall())
+        return
         
     def deleteVideo(self, tableName):
         "테이블을 지운다"
         delete_sql = """
-            DROP TABLE video_%s;
+            DROP TABLE video_%s
         """%(tableName)
         self.cursor.execute(delete_sql)
         self.conn.commit()
 
-if __name__ == '__main__':
-    ip = '192.168.0.12'
-    port = '1521'
-    id = 'bitai'
-    pw = 'bitai'
-    g_OracleDao = OracleDao(ips=ip, ports=port, ids=id, pws=pw)
+# if __name__ == '__main__':
+    # ip = '192.168.0.12'
+    # port = '1521'
+    # id = 'bitai'
+    # pw = 'bitai'
+    # tableName = "aaa"
+    # g_OracleDao = OracleDao(ips=ip, ports=port, ids=id, pws=pw)
+    # g_OracleDao.CreateTable(tableName=tableName)
+    # g_OracleDao.insertVideo(tableName=tableName, link="링크", time="000", title="title", views="views", origin="origin")
+    # g_OracleDao.deleteVideo(tableName=tableName)
     # g_OracleDao.searchVideo()
-    g_OracleDao.CreateTable()
